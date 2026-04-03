6542d8f9de7f6092a794d63ae9e41bed44ca913c
xBiltTom <biltonjr@hotmail.com>
2026-04-03 13:12:06 -0500

    Configuracion de variables de entorno con pydantic-settings listo

---
+++ app/core/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+#Base settings permite leer las variables de entorno, del sistema, etc.
+#SettingsConfigDict provee la configuracion extra para decirle como cargar esas variables.
+
+class Settings(BaseSettings): #Clase de configuración de la aplicación
+    #Información básica de la API
+    PROJECT_NAME: str = "User managment API"
+    VERSION: str = "1.0.0"
+    API_V1_STR: str = "/api/v1"
+    #Base de datos
+    DATABASE_URL: str
+    #Seguridad (JWT)
+    SECRET_KEY : str 
+    ALGORITHM : str 
+    ACCESS_TOKEN_EXPIRE_MINUTES : int
+    
+    #Configuracion para que pydantic lea el archivo .env
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", case_sensitive=True)
+    
+#Instanciamos la configuración para importarla en oiros archivos
+settings = Settings()
