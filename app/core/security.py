ba06e7b227ad481fc50d1c11bdddb65d2e9c318b
xBiltTom <biltonjr@hotmail.com>
2026-04-04 19:56:03 -0500

    Añadiendo logica de creacion de un jwt

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,8 @@
+from datetime import datetime, timedelta
+from typing import Any, Union
+from jose import jwt
+from app.core.config import settings
+
 import bcrypt
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -8,3 +13,14 @@ def get_password_hash(password: str) -> str:
     # Este método genera el hash de una contraseña
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
+def create_access_token(subject: Union[str,Any], expires_delta:timedelta = None) -> str :
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+        
+    to_encode = {"exp" : expire, "sub" : str(subject)}
+    encoded_jwt = jwt.encode(to_encode,settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
