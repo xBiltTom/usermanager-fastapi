0a7ca03cdedf1aedf46b8902c0e756f795e1e857
xBiltTom <biltonjr@hotmail.com>
2026-04-11 15:37:18 -0500

    Add test para validar que los usuario no autorizados no vean a los demás usuarios

--- tests/test_users.py
+++ tests/test_users.py
@@ -35,3 +35,39 @@ async def test_register_user(client: AsyncClient):
     # Verificación de seguridad: La contraseña nunca debe ser devuelta
     assert "password" not in data
     assert "hashed_password" not in data
+
+@pytest.mark.asyncio
+async def test_normal_user_cannot_read_all_users(client: AsyncClient):
+    """
+    Prueba que un usuario normal (no superusuario) recibe un error 403 Forbidden 
+    al intentar acceder a una ruta exclusiva de administradores.
+    """
+    # 1. Registramos a un usuario normal
+    user_data = {
+        "email" : "cursioso@testmail.com",
+        "full_name" : "Usuario Curioso",
+        "password" : "password123"
+    }
+    
+    await client.post("/api/v1/users/", json=user_data)
+    
+    # 2. Hacemos login para obtener su token
+    login_data = {
+        "username" : user_data["email"],
+        "password" : user_data["password"]
+    }
+    
+    login_response = await client.post("/api/v1/auth/login", data=login_data)
+    token = login_response.json()["access_token"]
+    
+    # 3. Intentamos acceder a la lista de todos los usarios (ruta protegida)
+    # Configuramos el header de autorización con el token bearer
+    headers = {"Authorization" : f"Bearer {token}"}
+    response = await client.get("/api/v1/users/", headers=headers)
+    
+    # 4. Verificaciones de seguridad
+    # 403 es un error http de falta de permisos para acceder a la ruta
+    assert response.status_code == 403
+    assert response.json()["detail"] == "El usuario no tiene suficientes privilegios"
+    
+    
